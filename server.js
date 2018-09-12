7043125d034b1345b4265033ef880bd3041a7e7e
pdadi <pdadi@miraclesoft.com>
2018-09-12 14:42:35 +0530

    test

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/',function(req,res){
+     res.sendFile(__dirname+'/'+'sample.html');
+
+});
+
+
+app.listen(process.env.port|| process.env.PORT||4000, function(){
+console.log("Server is running at 4000");
+});
